6e51dcda527737224245c5716da3b9cde4f5d4f5
lukenelson1012 <lanelson6@wisc.edu>
2025-06-03 10:12:24 -0400

    Loads data successfully

--- src/loan_default/etl/extract.py
+++ src/loan_default/etl/extract.py
@@ -1,6 +1,44 @@
 
 import kaggle as kg
+from pathlib import Path
+from pandas import DataFrame, read_csv
+from typing import Optional
+
+double_parent = Path("../../../data/anonymize_Loan_Default_data.csv")
+print(double_parent.exists())
+
+BASE_DATADIR = Path("./data/")
+BASE_DATA_FILENAME = Path("Anonymize_Loan_Default_data.csv")
+BASE_FILEPATH = BASE_DATADIR / BASE_DATA_FILENAME
+print(BASE_FILEPATH)
+
+def extract_data_from_kaggle(directory: Optional[Path] = None) -> None:
+
+    """
+    Requires API key
+    (talk about Kaggle, instructions, etc.)
+
+    """
+
+    if not directory:
+        directory = BASE_DATADIR
+
+    try:
+        kg.api.dataset_download_files(dataset="joebeachcapital/loan-default",
+                                           path=directory, 
+                                           unzip=True)
+    except Exception as e:
+        print(f"Make sure you have the API key. Error: {e}")
+        raise e
 
 
-file_path = "./data/"
-kg.api.dataset_download_files(dataset = "joebeachcapital/loan-default", path=file_path, unzip=True)
+def read_data(filepath: Optional[Path] = None) -> DataFrame:
+    if not filepath:
+        filepath = BASE_FILEPATH
+    if filepath.exists():
+        return read_csv(filepath)
+    else:
+        filepath.parent.mkdir(parents=True, exist_ok=True)
+        extract_data_from_kaggle(filepath.parent)
+        return read_csv(filepath, delimiter=',')
+
